7b91d6ce7e2c9703c09b59d8fe38e5426dd91d93
xugangfm <xugangfm@163.com>
2014-04-11 16:01:17 +0800

    修改主函数
    延时5秒

--- src/com/ailk/axis/Test.java
+++ src/com/ailk/axis/Test.java
@@ -20,7 +20,7 @@ public class Test {
 		try {
 			doERPNumber(client);
 			doERPBillList(client);
-			Thread.currentThread().sleep(1000);
+			Thread.currentThread().sleep(5000);
 			
 		} catch (Exception e) {
 			// TODO: handle exception
